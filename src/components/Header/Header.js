ec6b5255f093f5d56af92d8e89e64612ee0f612b
Nicholas Prendergast <nickprender@gmail.com>
2021-03-18 12:31:58 -0700

    Update heading

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,7 +19,7 @@ const Title = styled.header`
 export default function Header() {
   return (
     <Title>
-      <p>Checkers | Draughts Pull test</p>
+      <p>Checkers | Draughts</p>
     </Title>
   )
 }
